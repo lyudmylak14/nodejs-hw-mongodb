8843e9e685a830403ea1e5414e6b2bc53dd0739b
L-y-u-d-m-y-l-a <lyudmyla.k@hotmail.com>
2024-09-16 18:10:42 +0100

    create controllers

---
+++ src/controllers/contacts.js
@@ -0,0 +1,27 @@
+import { getAllContacts, getContactByID } from '../services/contacts.js';
+
+export const getContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactsByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactByID(contactId);
+
+  if (!contact) {
+    next(new Error('Contact not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,34 +1,12 @@
 import { Router } from 'express';
-import { getAllContacts, getContactByID } from '../services/contacts.js';
+import {
+  getContactsByIdController,
+  getContactsController,
+} from '../controllers/contacts.js';
 
 const router = Router();
 
-router.get('/contacts', async (req, res) => {
-  const contacts = await getAllContacts();
-
-  res.status(200).json({
-    status: 200,
-    message: 'Successfully found contacts!',
-    data: contacts,
-  });
-});
-
-router.get('/contacts/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await getContactByID(contactId);
-
-  if (!contact) {
-    res.status(404).json({
-      message: 'Contact not found',
-    });
-    return;
-  }
-
-  res.status(200).json({
-    status: 200,
-    message: `Successfully found contact with id ${contactId}!`,
-    data: contact,
-  });
-});
+router.get('/contacts', getContactsController);
+router.get('/contacts/:contactsId', getContactsByIdController);
 
 export default router;
