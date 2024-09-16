fb152d7797fcafb1037ad72dc802b4ac4276bc12
L-y-u-d-m-y-l-a <lyudmyla.k@hotmail.com>
2024-09-16 17:18:15 +0100

    creation of routers

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+// import contactsRouter from './routers/contacts.js';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactByID } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -23,35 +23,6 @@ export const setupServer = () => {
   app.get('/', (req, res) => {
     res.json({
       message: 'Welcome to home page!',
-    });
-  });
-
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-
-    const contact = await getContactByID(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
     });
   });
 
