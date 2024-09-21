608804d90209fc382fe18066915e82966a1b2f2e
L-y-u-d-m-y-l-a <lyudmyla.k@hotmail.com>
2024-09-21 13:26:41 +0100

    Refactor: replace next with throw for error handling

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -49,7 +49,7 @@ export const patchContactController = async (req, res, next) => {
   const contact = await updateContact(contactId, body);
 
   if (!contact) {
-    return next (createHttpError(404, 'Contact not found'));
+    throw (createHttpError(404, 'Contact not found'));
   }
 
   res.json({
@@ -64,8 +64,7 @@ export const deleteContactController = async (req, res, next) => {
   const contact = await deleteContact(contactId);
 
   if (!contact) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw (createHttpError(404, 'Contact not found'));
   }
 
   res.status(204).send();
